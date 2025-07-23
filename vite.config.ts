41e043227288b1c9fa039bbfffab52e2cfe579aa
rpleava@mobilecom.com <ryan.pleava@gmail.com>
2025-07-23 06:35:23 -0400

    fixed github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/mcc-landing-site/',
   plugins: [
     react(),
     tailwindcss(),
